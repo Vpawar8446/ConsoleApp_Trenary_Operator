ae0ec3aecf473a9963bf922f956bb7ce6b548662
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 22:51:10 +0530

    Add project files.

---
+++ ConsoleApp_Trenary_Operator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp_Trenary_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int balance = 10000, withdrawal;
+            string cust_name;
+            Console.Write("Enter the customer name: ");
+            cust_name = Convert.ToString(Console.ReadLine());
+            Console.Write("Enter Withdrawal Amount: ");
+            withdrawal = Convert.ToInt32(Console.ReadLine());
+            balance = withdrawal > balance ? balance : balance - withdrawal;
+            Console.WriteLine("Cusomer name: " + cust_name);
+            Console.Write("Remaining Balance: " + balance);
+        }
+           
+    }
+}
